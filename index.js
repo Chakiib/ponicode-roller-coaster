548f3d7b45e10f6556d1b863b331d67a9fdea996
Chakib BOUABD <cbouabd@nomadvantage.com>
2021-10-15 10:45:11 +0200

    Test for all samples

--- index.js
+++ index.js
@@ -29,10 +29,6 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
             // do/while since the attraction can only function for a limited number of times per day.
             do {
                 let numOfPersons = 0;
-                console.log('numOfPersons', numOfPersons);
-                console.log('groupCount', groupCount);
-                console.log('places', places);
-                console.log('numOfPersons + groupCount <= places', numOfPersons + groupCount <= places);
                 // People queue up in front of the attraction
                 // They can either be alone or in a group. When groups are in the queue, they necessarily want to ride together, without being separated.
                 // People never overtake each other in the queue.
